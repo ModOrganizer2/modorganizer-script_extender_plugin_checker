5ff911f518cc004264757b762c4128f10e6c0812
LostDragonist <lost.dragonist@gmail.com>
2019-03-26 14:48:32 -0500

    Rework description to add steps to follow and file origin

--- src/ScriptExtenderPluginChecker.py
+++ src/ScriptExtenderPluginChecker.py
@@ -10,47 +10,48 @@ if "mobase" not in sys.modules:
 
 
 class PluginMessage():
-    def __init__(self, pluginPath):
+    def __init__(self, pluginPath, organizer):
         self._pluginPath = Path(pluginPath)
-    
+        self._pluginOrigin = organizer.getFileOrigins(str(self._pluginPath.relative_to(organizer.managedGame().dataDirectory().absolutePath())))[-1]
+
     def successful(self):
         return False
-    
+
     def asMessage(self):
-        return self.__tr("{0} existed.").format(self._pluginPath.name)
-    
+        return self.__tr("{0} ({1}) existed.").format(self._pluginPath.name, self._pluginOrigin)
+
     def __tr(self, str):
         return QCoreApplication.translate("PluginMessage", str)
-    
+
     messageTypes = []
-    
+
     @staticmethod
     def registerMessageType(messageType):
         PluginMessage.messageTypes.append(messageType)
-    
+
     @staticmethod
-    def PluginMessageFactory(line):
+    def PluginMessageFactory(line, organizer):
         for messageType in PluginMessage.messageTypes:
             match = messageType[0].fullmatch(line)
             if match:
-                return messageType[1](match)
+                return messageType[1](match, organizer)
         return None
 
 
 class NormalPluginMessage(PluginMessage):
-    def __init__(self, match):
-        super(NormalPluginMessage, self).__init__(match.group("pluginPath"))
+    def __init__(self, match, organizer):
+        super(NormalPluginMessage, self).__init__(match.group("pluginPath"), organizer)
         self.__infoVersion = int(match.group("infoVersion"), 16)
         self.__name = match.group("name")
         self.__version = int(match.group("version"), 16)
         self.__loadStatus = match.group("loadStatus")
-    
+
     def successful(self):
         return self.__loadStatus == "loaded correctly"
-    
+
     def asMessage(self):
-        return self.__tr("{0} version {1} ({2}) {3}.").format(self.__name, self.__version, self._pluginPath.name, self.__trLoadStatus())
-    
+        return self.__tr("{0} version {1} ({2}, {4}) {3}.").format(self.__name, self.__version, self._pluginPath.name, self.__trLoadStatus(), self._pluginOrigin)
+
     def __trLoadStatus(self):
         # We need to list the possible options so they get detected as translatable strings.
         loadStatusTranslations = {
@@ -68,7 +69,7 @@ class NormalPluginMessage(PluginMessage):
             # There's a blacklist of known broken plugins that get excluded, each with their own reason.
             # We aren't translating that.
             return self.__loadStatus
-    
+
     def __tr(self, str):
         return QCoreApplication.translate("NormalPluginMessage", str)
 
@@ -76,16 +77,16 @@ PluginMessage.registerMessageType((re.compile(r"plugin (?P<pluginPath>.+) \((?P<
 
 
 class CouldntLoadPluginMessage(PluginMessage):
-    def __init__(self, match):
-        super(CouldntLoadPluginMessage, self).__init__(match.group("pluginPath"))
+    def __init__(self, match, organizer):
+        super(CouldntLoadPluginMessage, self).__init__(match.group("pluginPath"), organizer)
         self.__lastError = int(match.group("lastError"))
-    
+
     def successful(self):
         return False
-    
+
     def asMessage(self):
-        return self.__tr("Couldn't load {0}. The last error code was {1}.").format(self._pluginPath.name, self.__lastError)
-    
+        return self.__tr("Couldn't load {0} ({2}). The last error code was {1}.").format(self._pluginPath.name, self.__lastError, self._pluginOrigin)
+
     def __tr(self, str):
         return QCoreApplication.translate("CouldntLoadPluginMessage", str)
 
@@ -93,27 +94,27 @@ PluginMessage.registerMessageType((re.compile(r"couldn't load plugin (?P<pluginP
 
 
 class NotAPluginMessage(PluginMessage):
-    def __init__(self, match):
-        super(NotAPluginMessage, self).__init__(match.group("pluginPath"))
-    
+    def __init__(self, match, organizer):
+        super(NotAPluginMessage, self).__init__(match.group("pluginPath"), organizer)
+
     def successful(self):
         return False
-    
+
     def asMessage(self):
-        return self.__tr("{0} does not appear to be an SKSE plugin.").format(self._pluginPath.name)
-    
+        return self.__tr("{0} ({1}) does not appear to be a script extender plugin.").format(self._pluginPath.name, self._pluginOrigin)
+
     def __tr(self, str):
         return QCoreApplication.translate("NotAPluginMessage", str)
 
 PluginMessage.registerMessageType((re.compile(r"plugin (?P<pluginPath>.+) does not appear to be an (?:SK)|(?:F4)|(?:NV)|(?:FO)|(?:OB)SE plugin\s"), NotAPluginMessage))
 
-    
+
 class LogLocation(Enum):
     DOCS = auto()
     INSTALL = auto()
 
 class ScriptExtenderPluginChecker(mobase.IPluginDiagnose):
-    
+
     supportedGames = {
         "Skyrim" : (LogLocation.DOCS, Path("SKSE") / "skse.log"),
         "Skyrim Special Edition" : (LogLocation.DOCS, Path("SKSE") / "skse64.log"),
@@ -125,16 +126,16 @@ class ScriptExtenderPluginChecker(mobase.IPluginDiagnose):
         "TTW" : (LogLocation.INSTALL, Path("ttw_nvse.log")),
         "Fallout 3" : (LogLocation.INSTALL, Path("fose.log"))
     }
-    
+
     def __init__(self):
         super(ScriptExtenderPluginChecker, self).__init__()
         self.__organizer = None
 
     def init(self, organizer):
         self.__organizer = organizer
-        
+
         organizer.onFinishedRun(lambda a, b: self._invalidate())
-        
+
         return True
 
     def name(self):
@@ -155,79 +156,87 @@ class ScriptExtenderPluginChecker(mobase.IPluginDiagnose):
 
     def settings(self):
         return []
-    
+
     def activeProblems(self):
         if self.__scanLog():
             return [0]
         else:
             return []
-    
+
     def shortDescription(self, key):
         return self.__tr("Script extender log reports incompatible plugins.")
-    
+
     def fullDescription(self, key):
         pluginList = self.__listBadPluginMessagess()
-        pluginListString = "\n• " + ("\n• ".join(pluginList))
-        return self.__tr("You have one or more script extender plugins which failed to load. They are:{0}").format(pluginListString)
-    
+        pluginListString = "\n  • " + ("\n  • ".join(pluginList))
+        return self.__tr("You have one or more script extender plugins which failed to load!\n\n "
+        	             "If you want this notification to go away, here are some steps you can take:\n"
+        	             "  • Look for updates to the mod or the specific plugin included in the mod.\n"
+        	             "  • Disable the mod containing the plugin.\n"
+        	             "  • Hide or delete the plugin from the mod.\n\n"
+        	             "To refresh the script extender log, you will need to run the game again!\n\n"
+        	             "The failed plugins are:{0}").format(pluginListString)
+
     def hasGuidedFix(self, key):
         return False
-    
+
     def startGuidedFix(self, key):
         pass
-    
+
     def __tr(self, str):
         return QCoreApplication.translate("ScriptExtenderPluginChecker", str)
-    
+
     def __scanLog(self):
         if self.__organizer.managedGame().gameName() not in self.supportedGames:
             return False
-        
+
         base, suffix = self.supportedGames[self.__organizer.managedGame().gameName()]
-        
+
         if base == LogLocation.DOCS:
             base = Path(self.__organizer.managedGame().documentsDirectory().absolutePath())
         elif base == LogLocation.INSTALL:
             base = Path(self.__organizer.managedGame().gameDirectory().absolutePath())
-        
+
         logPath = base / suffix
-        
+
         try:
             with logPath.open('r') as logFile:
                 for line in logFile:
-                    pluginMessage = PluginMessage.PluginMessageFactory(line)
+                    pluginMessage = PluginMessage.PluginMessageFactory(line, self.__organizer)
                     if pluginMessage and not pluginMessage.successful():
                         return True
-        except Exception:
+        except Exception as e:
+            qDebug(str(e))
             # There's almost certainly just no log yet
             pass
-                    
+
         return False
-    
+
     def __listBadPluginMessagess(self):
         base, suffix = self.supportedGames[self.__organizer.managedGame().gameName()]
-        
+
         if base == LogLocation.DOCS:
             base = Path(self.__organizer.managedGame().documentsDirectory().absolutePath())
         elif base == LogLocation.INSTALL:
             base = Path(self.__organizer.managedGame().gameDirectory().absolutePath())
-        
+
         logPath = base / suffix
-                    
+
         messageList = []
-        
+
         try:
             with logPath.open('r') as logFile:
                 for line in logFile:
-                    pluginMessage = PluginMessage.PluginMessageFactory(line)
+                    pluginMessage = PluginMessage.PluginMessageFactory(line, self.__organizer)
                     if pluginMessage and not pluginMessage.successful():
                         messageList.append(pluginMessage.asMessage())
-        except Exception:
+        except Exception as e:
+            qDebug(str(e))
             # There's almost certainly just no log yet
             pass
-        
+
         return messageList
-                
-    
+
+
 def createPlugin():
     return ScriptExtenderPluginChecker()
